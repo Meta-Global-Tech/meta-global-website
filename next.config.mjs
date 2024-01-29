412026bb8f149cb156ea4ce3b671934e89470230
“Bawantha <bawanthaperera1995@gmail.com>
2024-01-29 23:21:02 +0530

    update deployemnt

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
   reactStrictMode: true,
+//   images: {unoptimized: true},
+//   experimental: {images: {unoptimized: true}},
+  basePath: "/meta-global-website"
 };
+
 
 export default nextConfig;
